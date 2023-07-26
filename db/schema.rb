d5b697558ec3fe1686fdb2784bd3199e935bdd9a
njmwas <njmwas@gmail.com>
2023-07-26 19:48:16 +0300

    updated develper environment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2023_04_11_105501) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.integer "member_id"
     t.integer "total_savings"
